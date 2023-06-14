30866e027f8c26ab55cd712898b06099864cda4c
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-06-14 17:44:49 +0100

    refactor: Format file

--- src/hooks/useToggle/__test__/useToggle.test.tsx
+++ src/hooks/useToggle/__test__/useToggle.test.tsx
@@ -1,32 +1,31 @@
-import { describe, it, expect, beforeEach, afterEach } from 'vitest';
-import { render, screen, cleanup } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import UseToggleWrapper from './UseToggleWrapper';
+import { describe, it, expect, beforeEach, afterEach } from "vitest";
+import { render, screen, cleanup } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import UseToggleWrapper from "./UseToggleWrapper";
 
 function renderWrapper() {
   render(<UseToggleWrapper />);
 }
 
-
-describe('useToggleState', () => {
+describe("useToggleState", () => {
   beforeEach(() => renderWrapper());
   afterEach(() => cleanup());
-  
-  it('returns an initial state of null', () => {
-    const stateDisplay = screen.getByText('null');
+
+  it("returns an initial state of null", () => {
+    const stateDisplay = screen.getByText("null");
 
     expect(stateDisplay).toBeTruthy();
   });
 
-  it('allows the state to be toggled', async () => {
-    const toggleBtn = screen.getByText('toggleState');
+  it("allows the state to be toggled", async () => {
+    const toggleBtn = screen.getByText("toggleState");
 
     await userEvent.click(toggleBtn);
 
-    expect(screen.getByText('true')).toBeTruthy();
+    expect(screen.getByText("true")).toBeTruthy();
 
     await userEvent.click(toggleBtn);
 
-    expect(screen.getByText('false')).toBeTruthy();
+    expect(screen.getByText("false")).toBeTruthy();
   });
 });
