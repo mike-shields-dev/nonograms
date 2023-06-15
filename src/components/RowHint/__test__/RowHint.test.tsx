60002fbca04963a7273b2aa7a865476fd24d2eaa
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-06-15 11:22:43 +0100

    test/fix: Remove old rowHint prop from RowHint comp render in RowHint test

--- src/components/RowHint/__test__/RowHint.test.tsx
+++ src/components/RowHint/__test__/RowHint.test.tsx
@@ -1,11 +1,11 @@
 import { describe, it, expect } from "vitest";
-import { render, screen, cleanup } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import RowHint from "../RowHint";
 
 describe("RowHint", () => {
   it("renders an element with the correct value for each count in rowHint prop", () => {
     const rowHint = [1, 2, 3, 4, 5, 6];
-    render(<RowHint rowHint={rowHint} rowIndex={1} />);
+    render(<RowHint rowHint={rowHint} />);
     
     rowHint.forEach((count, i) => {
       const expectedCount = i < rowHint.length - 1 ? `${count},` : count;
