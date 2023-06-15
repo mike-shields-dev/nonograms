ccab6e524e4bf090610cb7102ba0188d8868d77c
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-06-15 09:08:24 +0100

    chore: App component determines --grid-resolution CSS property based on matrix size

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { useEffect } from "react";
+import levels from "./assets/levels.json";
+
 import Board from './components/Board/Board';
 import levels from './assets/levels.json';
 import './App.css'
 
 function App() {
   const levelIndex = 0;
-  
+  const matrix = levels[levelIndex];
+
+  useEffect(() => {
+    const root = document.documentElement;
+    root.style.setProperty("--grid-resolution", `${matrix.length}`);
+  }, [matrix]);
+
   return (
     <>
       <main>
