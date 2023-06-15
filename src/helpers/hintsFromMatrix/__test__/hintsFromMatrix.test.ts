16d1d00eb9a8baf1e280e96f16fc22711c9c18ed
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-06-15 09:02:21 +0100

    chore: Create hintsFromMatrix function that counts matrix row groupings of true values

---
+++ src/helpers/hintsFromMatrix/__test__/hintsFromMatrix.test.ts
@@ -0,0 +1,34 @@
+import { describe, it, expect } from "vitest";
+import hintsFromMatrix from "../hintsFromMatrix";
+
+const matrices = [
+  [
+    [true, false],
+    [true, false],
+  ],
+  [
+    [true, false, true],
+    [true, true, true],
+    [false, true, false],
+  ],
+  [
+    [true, false, true, true],
+    [false, true, true, false],
+    [true, true, true, false],
+    [false, true, false, false],
+  ],
+];
+
+const hints = [
+  [[1], [1]],
+  [[1, 1], [3], [1]],
+  [[1, 2], [2], [3], [1]],
+];
+
+describe("hintsFromMatrix", () => {
+  it("returns a number representing each group of consecutive true values in a matrix row", () => {
+    matrices.forEach((matrix, i) => {
+      expect(hintsFromMatrix(matrix)).toStrictEqual(hints[i]);
+    });
+  });
+});
