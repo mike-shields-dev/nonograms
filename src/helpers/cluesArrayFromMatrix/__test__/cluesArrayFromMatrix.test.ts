5350a870d1061da61580b8b6477e60692d814857
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-06-16 14:21:47 +0100

    refactor: Refactor cluesArrayFromMatrix test

--- src/helpers/cluesArrayFromMatrix/__test__/cluesArrayFromMatrix.test.ts
+++ src/helpers/cluesArrayFromMatrix/__test__/cluesArrayFromMatrix.test.ts
@@ -1,41 +1,50 @@
 import { describe, it, expect } from "vitest";
 import cluesArrayFromMatrix from "../cluesArrayFromMatrix";
 
-const matrices = [
-  [
-    [true, false],
-    [true, false],
-  ],
-  [
-    [true, false, true],
-    [true, true, true],
-    [false, true, false],
-  ],
-  [
-    [true, false, true, true],
-    [false, true, true, false],
-    [true, true, true, false],
-    [false, true, false, false],
-  ],
-  [
-    [true, false, true, true],
-    [false, true, true, false],
-    [true, true, true, false],
-    [false, false, false, false],
-  ],
-];
+const testCases = [
+  {
+    input: [
+      [true, false],
+      [true, false],
+    ],
 
-const hints = [
-  [[1], [1]],
-  [[1, 1], [3], [1]],
-  [[1, 2], [2], [3], [1]],
-  [[1, 2], [2], [3], []],
+    expected: [[1], [1]],
+  },
+  {
+    input: [
+      [true, false, true],
+      [true, true, true],
+      [false, true, false],
+    ],
+
+    expected: [[1, 1], [3], [1]],
+  },
+  {
+    input: [
+      [true, false, true, true],
+      [false, true, true, false],
+      [true, true, true, false],
+      [false, true, false, false],
+    ],
+
+    expected: [[1, 2], [2], [3], [1]],
+  },
+  {
+    input: [
+      [true, false, true, true],
+      [false, true, true, false],
+      [true, true, true, false],
+      [false, false, false, false],
+    ],
+
+    expected: [[1, 2], [2], [3], []],
+  },
 ];
 
 describe("cluesArrayFromMatrix", () => {
-  it("returns a number representing each group of consecutive true values in a matrix row", () => {
-    matrices.forEach((matrix, i) => {
-      expect(cluesArrayFromMatrix(matrix)).toStrictEqual(hints[i]);
+  it("returns a number for each group of consecutive true values in a matrix row", () => {
+    testCases.forEach((testCase) => {
+      expect(cluesArrayFromMatrix(testCase.input)).toStrictEqual(testCase.expected);
     });
   });
 });
