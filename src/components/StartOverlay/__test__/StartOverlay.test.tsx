0803e3999f50dbf781cbd69252b5d42eed598987
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-06-18 21:28:34 +0100

    test: Assert that the StartOverlay component conditionally renders based on show prop

--- src/components/StartOverlay/__test__/StartOverlay.test.tsx
+++ src/components/StartOverlay/__test__/StartOverlay.test.tsx
@@ -7,7 +7,7 @@ const spy = vi.fn();
 
 describe("StartOverlay", () => {
   it("should display a single button", () => {
-    render(<StartOverlay onClick={spy} />);
+    render(<StartOverlay onClick={spy} show={true} />);
 
     expect(screen.getAllByRole("button")).toHaveLength(1);
 
@@ -15,7 +15,7 @@ describe("StartOverlay", () => {
   });
 
   it("should display a button with the text matching /start/i", () => {
-    render(<StartOverlay onClick={spy} />);
+    render(<StartOverlay onClick={spy} show={true} />);
 
     expect(screen.getByRole("button").textContent).toMatch(/start/i);
 
@@ -23,12 +23,18 @@ describe("StartOverlay", () => {
   });
 
   it("should invoke it's click handler when clicked", async () => {
-    render(<StartOverlay onClick={spy} />);
+    render(<StartOverlay onClick={spy} show={true} />);
 
     await userEvent.click(screen.getByRole("button"));
 
     expect(spy).toHaveBeenCalledTimes(1);
 
     cleanup();
+  });
+
+  it("should not render anything if show prop is false", () => {
+    render(<StartOverlay onClick={spy} show={false} />);
+
+    expect(screen.queryByText(/start/i)).toBeNull();
   });
 });
