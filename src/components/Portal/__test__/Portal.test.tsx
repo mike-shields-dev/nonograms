aacc035c804a751e51036e6955c8bc1855945363
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-06-18 13:51:11 +0100

    test/fix: Refactor Portal test to fix React error (lists and keys)

--- src/components/Portal/__test__/Portal.test.tsx
+++ src/components/Portal/__test__/Portal.test.tsx
@@ -2,7 +2,11 @@ import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import Portal from "../Portal";
 
-const children = [<div>CHILD1</div>, <div>CHILD2</div>];
+const children = (
+  <>
+    <div>CHILD1</div>,<div>CHILD2</div>;
+  </>
+);
 
 describe("Portal", () => {
   it("creates a portal and renders it's children within", () => {
