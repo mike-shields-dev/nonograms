2c476d3d4fbb9be3c396d58930ef2883fd0d44ae
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-06-19 08:38:55 +0100

    refactor: Remove vi obj from RunningTimeDisplay test

--- src/components/RunningTimeDisplay/__test__/RunningTimeDisplay.test.tsx
+++ src/components/RunningTimeDisplay/__test__/RunningTimeDisplay.test.tsx
@@ -1,4 +1,4 @@
-import { describe, expect, it, vi } from "vitest";
+import { describe, expect, it } from "vitest";
 import { cleanup, render, screen } from "@testing-library/react";
 
 import RunningTimeDisplay from "../RunningTimeDisplay";
